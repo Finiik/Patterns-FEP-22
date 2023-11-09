f3fe20a1939253d815cacec954385bbb9daa9d70
TarasVoy <115774578+TarasVoy@users.noreply.github.com>
2023-11-09 08:37:26 +0200

    port.py

--- voitseshchuk/papka lab№2/port.py
+++ voitseshchuk/papka lab№2/port.py
@@ -15,15 +15,14 @@ class Port:
 
         self
 def load_container(self, container):
-        # Додати логіку для завантаження контейнера в порт
+   
         pass
 
     def unload_container(self, container):
-        # Додати логіку для розвантаження контейнера з порту
         
        
 pass
 
     def find_closest_refueling_port(self):
-        # Додати логіку для пошуку найближчого порту для підйому палива
+       
         pass
