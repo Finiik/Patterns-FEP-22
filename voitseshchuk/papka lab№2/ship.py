8de4bf56b791258457a901f7161ee95f3a77666f
TarasVoy <115774578+TarasVoy@users.noreply.github.com>
2023-11-09 08:33:29 +0200

    Create ship.py

---
+++ voitseshchuk/papka lab№2/ship.py
@@ -0,0 +1,23 @@
+class Ship:
+    def __init__(self, ID, port, max_weight, max_num_containers, max_num_heavy_containers, max_num_refrigerated, max_num_liquid, fuel_consumption):
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self.max_num_liquid = max_num_liquid
+        self.fuel_consumption = fuel_consumption
+        self.containers = []
+
+    def load_container(self, container):
+        # Додати логіку для перевірки, чи може контейнер бути завантажений на корабель
+        pass
+
+    def unload_container(self, container):
+        # Додати логіку для розвантаження контейнера з корабля
+        pass
+
+    def travel(self, destination_port):
+        # Додати логіку для подорожі до іншого порту
+        pass
