3c8d114ff9e72883e7b217a3d33b8d18e9e290b0
TarasVoy <115774578+TarasVoy@users.noreply.github.com>
2023-11-09 08:34:40 +0200

    ship.py

--- voitseshchuk/papka lab№2/ship.py
+++ voitseshchuk/papka lab№2/ship.py
@@ -10,11 +10,105 @@ class Ship:
         self.fuel_consumption = fuel_consumption
         self.containers = []
 
-    def load_container(self, container):
+    
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self.max_num_liquid = max_num_liquid
+        self.fuel_consumption = fuel_consumption
+        self.containers =
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self.max_num_liquid = max_num_liquid
+        self.fuel_consumption = fuel_consumption
+        self.containers
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self.max_num_liquid = max_num_liquid
+       
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self.max_num_liquid = max_num_liquid
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated = max_num_refrigerated
+        self
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+        self.max_num_refrigerated
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers = max_num_heavy_containers
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy_containers
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_heavy
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num_he
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_containers
+        self.max_num
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_containers = max_num_cont
+
+        self.ID = ID
+        self.port = port
+        self.max_weight = max_weight
+        self.max_num_cont
+def load_container(self, container):
         # Додати логіку для перевірки, чи може контейнер бути завантажений на корабель
         pass
 
-    def unload_container(self, container):
+    
+
+def unload_container(self, container):
         # Додати логіку для розвантаження контейнера з корабля
         pass
 
