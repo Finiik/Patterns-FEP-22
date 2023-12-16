dc023803ee6920f3dcad316c8f30803604584e0a
Oleh Bezrodnyi <oleh.bezrodnyi@lnu.edu.ua>
2023-12-16 02:09:45 +0200

    -

--- bezrodnyi/Lab_4/amazing_api/app/api/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routes.ships import router as ship_router
-from app.api.routes.ports import router as ports_router
-
-router = APIRouter()
-router.include_router(ports_router, prefix="/ports", tags=["ports"])
-router.include_router(ship_router, prefix="/ships", tags=["ships"])
--- bezrodnyi/Lab_4/amazing_api/app/schemas/containers.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-from pydantic import BaseModel
-
-
-class Container(BaseModel, ABC):
-    pass
